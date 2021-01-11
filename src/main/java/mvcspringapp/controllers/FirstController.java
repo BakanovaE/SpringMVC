b27c96839e97e7011e2b0dceff6d84dad97fcb93
Ekaterina Bakanova <k125598@yandex.ru>
2021-01-11 16:56:42 +0300

    Model. Simple web clculator

--- src/main/java/mvcspringapp/controllers/FirstController.java
+++ src/main/java/mvcspringapp/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package mvcspringapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,11 +12,41 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/first")
 public class FirstController {
 
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam(value = "a") int a,
+                             @RequestParam(value = "b") int b,
+                             @RequestParam(value = "action") String action,
+                             Model model) {
+        double result;
+
+        switch(action) {
+            case "multiplication":
+            result = a*b;
+            break;
+            case "division":
+                result = a/(double)b;
+                break;
+            case "substraction":
+                result = a-b;
+                break;
+            case "addition" :
+                result = a+b;
+                break;
+            default:
+                result = 0;
+                break;
+        }
+        model.addAttribute("result", result);
+        return "first/calculator";
+    }
+
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname" , required = false) String surname) {
+                            @RequestParam(value = "surname" , required = false) String surname,
+                            Model model) {
 
-        System.out.println("Hello, " + name + " " + surname);
+        //System.out.println("Hello, " + name + " " + surname);
+        model.addAttribute("message", "Hello, " + name + " " + surname);
 
         return "first/hello";
     }
